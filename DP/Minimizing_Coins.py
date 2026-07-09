0ad7dc415050262b1e90087fb91ece66ab73f318
DiviJakhotiya <divyjakhotiya@gmail.com>
2026-07-10 00:32:21 +0530

    new_problem

---
+++ DP/Minimizing_Coins.py
@@ -0,0 +1,12 @@
+n, x = map(int, input().split())
+coins = list(map(int, input().split()))
+INF = 10**9
+dp = [INF] * (x + 1)
+dp[0] = 0
+for c in coins:
+    for i in range(c, x + 1):
+        if dp[i - c] + 1 < dp[i]:
+            dp[i] = dp[i - c] + 1
+print(dp[x] if dp[x] != INF else -1)
+
+#Solved
