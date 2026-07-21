58b3d27e56d109d2a8bc5d9c5c1675ce1e038213
DiviJakhotiya <divyjakhotiya@gmail.com>
2026-07-21 19:24:23 +0530

    Completely correct logic but tle's for python for obvious reasons

---
+++ DP/Book_Shop.py
@@ -0,0 +1,27 @@
+# n , k = map(int , input().split())
+# F = list(map(int , input().split()))
+# G = list(map(int , input().split()))
+# dp = [0] * (k+1)
+# dp[0] = 0
+# for i in range(n):
+#     for j in range(k , F[i] -1 , -1):
+#         dp[j] = max(dp[j] , G[i] + dp[j - F[i]])
+# print(dp[k])
+#TLE CODE
+#I did think of iterating over sun(G) but contraint of sum under xyz not mentioned in the question leading me 
+#to change the approach to what it was above ^
+n , k = map(int , input().split())
+F = list(map(int , input().split()))
+G = list(map(int , input().split()))
+INF = 10**18
+max_val = sum(G)
+dp = [INF] * (max_val + 1)
+dp[0] = 0
+for i in range(n):
+    for v in range(max_val, G[i]-1, -1):
+        dp[v] = min(dp[v], dp[v - G[i]] + F[i])
+ans = 0
+for v in range(max_val + 1):
+    if dp[v] <= k:
+        ans = v
+print(ans)
