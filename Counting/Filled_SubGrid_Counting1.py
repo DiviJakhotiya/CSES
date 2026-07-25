a493854c7aff7e414ad75bc107da0d6927122df0
DiviJakhotiya <divyjakhotiya@gmail.com>
2026-07-26 02:38:17 +0530

    I had a n^2*26 solution for it but CSES being annoying didn't pass will pass for cpp

---
+++ Counting/Filled_SubGrid_Counting1.py
@@ -0,0 +1,30 @@
+# 5 3
+# ABBBC
+# BBBBC
+# BCAAA
+# AAAAA
+# AAAAA
+
+n, k = map(int, input().split())
+grid = [list(input().strip()) for _ in range(n)]
+
+for c in range(k):
+    ch = chr(ord('A') + c)
+
+    dp = [[0]*n for _ in range(n)]
+    res = 0
+
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == ch:
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = min(
+                        dp[i-1][j],
+                        dp[i][j-1],
+                        dp[i-1][j-1]
+                    ) + 1
+                res += dp[i][j]
+
+    print(res)
